b4e92ec909607e2fff06e5888ccdd1c6a5d1deac
m0ribe <moro.recr@gmail.com>
2020-07-01 12:39:47 +0900

    add "redirect index" unlsee signed in?

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,13 @@
 class ProductsController < ApplicationController
+  before_action :move_to_index, except: [:index]
+  # before_action :move_to_index, except: [:index, :show] show実装後に上と切り替え
+
   def index
   end
+
+  private
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
+  end
+
 end
