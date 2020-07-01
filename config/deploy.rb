9e82afb345fb5b03eaf77aa80a54f3066a718f38
m0ribe <moro.recr@gmail.com>
2020-07-01 19:12:18 +0900

    Update deploy.rb
    Make deploy available using multiple branches.

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,6 +4,7 @@ lock '3.14.1'
 # 自身のアプリ名、リポジトリ名を記述
 set :application, 'fleamarket_sammple_78b'
 set :repo_url,  'git@github.com:m0ribe/fleamarket_sammple_78b.git'
+set :branch, ENV["BRANCH"] || "master"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
