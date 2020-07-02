046dbdae0f75f0d0dacb01d23938417ff5139a57
m0ribe <moro.recr@gmail.com>
2020-07-02 19:35:10 +0900

    Basic authentication is fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth, if: :production?
   before_action :basic_auth
+  before_action :basic_auth, if: :production?
 
   private
 
@@ -10,7 +10,8 @@ class ApplicationController < ActionController::Base
   
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+      username == Rails.application.credentials[:basic_auth][:user] &&
+      password == Rails.application.credentials[:basic_auth][:pass]
     end
   end
 end
