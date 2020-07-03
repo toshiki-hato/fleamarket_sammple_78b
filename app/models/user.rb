1970dc604f656acf38fadccc6b84c703b6498044
m0ribe <moro.recr@gmail.com>
2020-07-03 18:09:39 +0900

    add validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,22 @@ class User < ApplicationRecord
 
   before_save { self.email = email.downcase }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, {presence: true, , format: { with: VALID_EMAIL_REGEX }}
+  validates :email, {format: { with: VALID_EMAIL_REGEX }}
   VALID_FIRST_NAME_REGEX = /\A[一-龥ぁ-ん]/
-  validates :first_name, {presence: true, , format: { with: VALID_EMAIL_REGEX }}
-
-  validates :nickname, presence: true, length: { minimum: 7 }
+  validates :first_name, {format: { with: VALID_FIRST_NAME_REGEX }}
+  VALID_LAST_NAME_REGEX = /\A[一-龥ぁ-ん]/
+  validates :last_name, {format: { with: VALID_LAST_NAME_REGEX }}
+  VALID_FIRST_NAME_PHONETIC_REGEX = /\A[ぁ-んー－]+\z/
+  validates :first_name_phonetic, {format: { with: VALID_FIRST_NAME_PHONETIC_REGEX }}
+  VALID_LAST_NAME_PHONETIC_REGEX = /\A[ぁ-んー－]+\z/
+  validates :last_name_phonetic, {format: { with: VALID_FIRST_NAME_PHONETIC_REGEX }}
+  validates :nickname, length: { minimum: 7 }
+  VALID_SEND_FIRST_NAME_REGEX = /\A[一-龥ぁ-ん]/
+  validates :send_first_name, {format: { with: VALID_SEND_FIRST_NAME_REGEX }}
+  VALID_SEND_LAST_NAME_REGEX = /\A[一-龥ぁ-ん]/
+  validates :send_last_name, {format: { with: VALID_SEND_LAST_NAME_REGEX }}
+  VALID_SEND_FIRST_NAME_PHONETIC_REGEX = /\A[ぁ-んー－]+\z/
+  validates :send_first_name_phonetic, {format: { with: VALID_SEND_FIRST_NAME_PHONETIC_REGEX }}
+  VALID_SEND_LAST_NAME_PHONETIC_REGEX = /\A[ぁ-んー－]+\z/
+  validates :send_last_name_phonetic, {format: { with: VALID_SEND_FIRST_NAME_PHONETIC_REGEX }}
 end
