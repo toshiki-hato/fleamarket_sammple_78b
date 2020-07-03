27c06bff080cfad5eb8ef0f93679d10c225cb97b
m0ribe <moro.recr@gmail.com>
2020-07-03 17:25:05 +0900

    validation is added

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  before_save { self.email = email.downcase }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, {presence: true, , format: { with: VALID_EMAIL_REGEX }}
+  VALID_FIRST_NAME_REGEX = /\A[一-龥ぁ-ん]/
+  validates :first_name, {presence: true, , format: { with: VALID_EMAIL_REGEX }}
+
+  validates :nickname, presence: true, length: { minimum: 7 }
 end
