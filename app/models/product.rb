8bcd652a2c2b10cf5364784296e4a0798574aa20
ryotinjpn <ryotinjpn@gmail.com>
2020-07-03 17:17:22 +0900

    add category model

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,7 +4,7 @@ class Product < ApplicationRecord
   has_many :product_images
   accepts_nested_attributes_for :product_images, allow_destroy: true
   #belongs_to :user
-  #belongs_to :category
+  belongs_to :category
 
   validates :name,              presence: true
   validates :description,       presence: true
