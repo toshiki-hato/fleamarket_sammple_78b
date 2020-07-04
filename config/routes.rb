56efbcd60a924b5b658ef55dde74434ba377e69b
m0ribe <moro.recr@gmail.com>
2020-07-04 22:27:12 +0900

    user route check

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users, only: [:edit, :update, :index, :show]
   root to: 'products#index'
-  resources :users, only: [:show, :destroy]
+  
 end
