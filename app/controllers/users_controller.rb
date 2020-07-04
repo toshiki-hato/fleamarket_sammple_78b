ea05aea9042d75706f623a3287deada15b7825d7
m0ribe <moro.recr@gmail.com>
2020-07-05 02:36:27 +0900

    routes setting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,7 @@ class UsersController < ApplicationController
   def edit
   end
 
+  def destroy
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: [:edit, :update, :index, :show]
   root to: 'products#index'
+  resources :users, only: [:edit, :update, :index, :show] do
+    resources :credit_card, only: :new
+  end
   
 end
