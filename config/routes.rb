6e33fcac0501218f4756e9ae5d51484f74e1ef46
m0ribe <moro.recr@gmail.com>
2020-07-06 18:46:18 +0900

    routes setting buy

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   resources :users, only: [:edit, :update, :index, :show] do
     resources :credit_card, only: :new
   end
-  resources :products
+  resources :products do
+    collection do
+      get :buy
+    end
+  end
 end
