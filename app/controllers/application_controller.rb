bf916911ae7bc49962996e560a71822b651c0daf
ryotinjpn <ryotinjpn@gmail.com>
2020-07-06 12:17:00 +0900

    fix registrations/new.html.haml link_to image_tag

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :basic_auth
   before_action :basic_auth, if: :production?
-  protected
 
+  protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :first_name_phonetic, :last_name_phonetic, :birthday, :nickname, :email, :send_first_name, :send_last_name, :send_first_name_phonetic, :send_last_name_phonetic, :postal_code, :prefectures, :city, :address, :condominuium_name, :phone_number])
   end
