73218a3c76d62c1ec205775f52405ba6ec853cf4
m0ribe <moro.recr@gmail.com>
2020-07-06 10:21:56 +0900

    password encryption

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
-  # protect_from_forgery with: :exception
-  # before_action :authenticate_user!
+  protect_from_forgery with: :exception
+  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :first_name_phonetic, :last_name_phonetic, :birthday, :nickname, :email, :send_first_name, :send_last_name, :send_first_name_phonetic, :send_last_name_phonetic, :postal_code, :prefectures, :city, :address, :condominuium_name, :phone_number])
   end
 end
 
