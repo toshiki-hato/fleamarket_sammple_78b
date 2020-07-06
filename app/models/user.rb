46a2cb53d8b6e35dcf4b200473e3ab9f2110f5d2
m0ribe <moro.recr@gmail.com>
2020-07-06 17:59:55 +0900

    change validation
    remove valid nickname
    add balid password

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,7 +31,8 @@ class User < ApplicationRecord
   validates :first_name_phonetic, {format: { with: VALID_FIRST_NAME_PHONETIC_REGEX }}
   VALID_LAST_NAME_PHONETIC_REGEX = /\A[ぁ-んー－]+\z/
   validates :last_name_phonetic, {format: { with: VALID_FIRST_NAME_PHONETIC_REGEX }}
-  validates :nickname, length: { minimum: 7 }
+  validates :password, length: { minimum: 7 }
+  validates :encrypted_password, length: { minimum: 7 }
   VALID_SEND_FIRST_NAME_REGEX = /\A[一-龥ぁ-ん]/
   validates :send_first_name, {format: { with: VALID_SEND_FIRST_NAME_REGEX }}
   VALID_SEND_LAST_NAME_REGEX = /\A[一-龥ぁ-ん]/
