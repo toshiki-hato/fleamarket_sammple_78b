8dabd1bdd2f0261db2573c6e1cff0df7757a95b2
toshiki-hato <66351093+toshiki-hato@users.noreply.github.com>
2020-07-07 14:46:55 +0900

    mypage 編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :users
 
   validates :first_name,              presence: true
   validates :last_name,              presence: true
