4ea3377b0a5ebcff4e645c26dd34cc1224a82982
JijiKusahara <kayokawase0519@gmail.com>
2020-07-09 11:48:21 +0900

    Comment out Delete

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,21 +1,21 @@
-# require 'carrierwave/storage/abstract'
-# require 'carrierwave/storage/file'
-# require 'carrierwave/storage/fog'
+require '/storage/abstract'
+require '/storage/file'
+require '/storage/fog'
 
-# CarrierWave.configure do |config|
-#   if Rails.env.development? || Rails.env.test?
-#     config.storage = :file
-#   else
-#     config.storage = :fog
-#     config.fog_provider = 'fog/aws'
-#     config.fog_credentials = {
-#       provider: 'AWS',
-#       aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-#       aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-#       region: 'ap-northeast-1'
-#     }
+.configure do |config|
+  if Rails.env.development? || Rails.env.test?
+    config.storage = :file
+  else
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+      region: 'ap-northeast-1'
+    }
 
-#     config.fog_directory  = 'rails-78b'
-#     config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/rails-78b'
-#   end
-# end
+    config.fog_directory  = 'rails-78b'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/rails-78b'
+  end
+end
