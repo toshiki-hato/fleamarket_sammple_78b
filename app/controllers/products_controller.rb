fe64b02b4cff3bcee3704fb2c374dc35b44ed141
m0ribe <moro.recr@gmail.com>
2020-07-11 00:49:51 +0900

    buy route method changed

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
   before_action :set_product, except: [:index, :new, :create]
   before_action :move_to_index, except: [:index, :show]
-  before_action :set_card, except: [:buy, :pay]
+  before_action :set_card, only: [:buy, :pay]
 
   def index
     @products = Product.all.includes(:product_images).limit(4).shuffle
@@ -54,7 +54,7 @@ class ProductsController < ApplicationController
   def buy
     #Cardテーブルは前回記事で作成、テーブルからpayjpの顧客IDを検索
     @product = Product.find(params[:id])
-    if card.blank?
+    if @card.blank?
       #登録された情報がない場合にカード登録画面に移動
       redirect_to new_credit_card_path
     elsif @product.order == "購入済"
@@ -62,9 +62,9 @@ class ProductsController < ApplicationController
     else
       Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
       #保管した顧客IDでpayjpから情報取得
-      customer = Payjp::Customer.retrieve(card.customer_id)
+      customer = Payjp::Customer.retrieve(@card.customer_id)
       #保管したカードIDでpayjpから情報取得、カード情報表示のためインスタンス変数に代入
-      @default_card_information = customer.cards.retrieve(card.card_id)
+      @default_card_information = customer.cards.retrieve(@card.card_id)
     end
   end
 
@@ -72,7 +72,7 @@ class ProductsController < ApplicationController
     Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
     Payjp::Charge.create(
     amount: Product.find(1).price, #支払金額を入力（itemテーブル等に紐づけても良い）
-    customer: card.customer_id, #顧客ID
+    customer: @card.customer_id, #顧客ID
     currency: 'jpy', #日本円
   )
   redirect_to action: 'done' #完了画面に移動
@@ -109,6 +109,6 @@ class ProductsController < ApplicationController
     redirect_to action: :index unless user_signed_in?
   end
   def set_card
-    card = CreditCard.where(user_id: current_user.id).first
+    @card = CreditCard.where(user_id: current_user.id).first
   end
 end
