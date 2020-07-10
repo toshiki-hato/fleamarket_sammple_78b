3a463913ba17f05ef5da48a10150f714278052ce
m0ribe <moro.recr@gmail.com>
2020-07-10 18:51:09 +0900

    Revert "Revert "products controller add suffle""
    This reverts commit dcb354069443cac1bc73281ade26accd2efedd64.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,7 +3,7 @@ class ProductsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
 
   def index
-    @products = Product.all.includes(:product_images).limit(4)
+    @products = Product.all.includes(:product_images).limit(4).shuffle
   end
 
   def show
