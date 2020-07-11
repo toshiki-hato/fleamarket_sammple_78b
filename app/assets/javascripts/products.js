9635d84f467f53ca1f2f404c6175612091222335
Yayuta <66344846+Yayuta@users.noreply.github.com>
2020-07-11 14:52:15 +0900

    add css

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,13 +1,11 @@
 $(document).on('turbolinks:load', ()=> {
   // 画像用のinputを生成する関数
   const buildFileField = (num)=> {
-    const html = `<label class="label">
-                  <div class="image-btn">アップロード</div>
-                  <div data-index="${num}" class="js-file_group">
-                      <input class="js-file file-btn" type="file"
-                      name="product[product_images_attributes][${num}][image]"
-                      id="product_images_attributes_${num}_image"></label><br>
-                    <div class="js-remove">削除</div>
+    const html = `<div data-index="${num}" class="js-file_group">
+                    <input class="js-file" type="file"
+                    name="product[product_images_attributes][${num}][image]"
+                    id="product_images_attributes_${num}_image"><br>
+                    <button class="js-remove">削除</button>
                   </div>`;
     return html;
   }
